1f23e4c6e5c2caa17db928988a5103b7407916a0
majkel355 <129754260+majkel355@users.noreply.github.com>
2026-05-01 14:29:31 +0200

    enemy, textures and script
    I created a sphere, named it "opponent", added a texture to it, created an empty object, named it "spawn manager", created two new scripts, one to spawn enemies in a random location and connected it to the "spawn manager" object, the second one is a script that allows the enemy to move towards the player, created a physics material, named it "bouncy" and connected it to the player and the enemy

---
+++ Assets/scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefabs;
+    private float spawnRange = 9;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Instantiate(enemyPrefabs, GenerateSpawnPosition(), enemyPrefabs.transform.rotation);
+    }
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
